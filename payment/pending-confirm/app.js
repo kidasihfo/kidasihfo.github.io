5dc0e3c013bd6bfa3800e42c1ddad4ae013219a5
kidasihfo <163608650+kidasihfo@users.noreply.github.com>
2024-07-04 01:52:44 +0700

    Create app.js

---
+++ payment/pending-confirm/app.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const clientName = urlParams.get('client');
+    const amount = urlParams.get('rp') ? parseInt(urlParams.get('rp'), 10) : null;
+
+    if (!clientName || !amount) {
+        window.location.href = '../404.html';
+    }
+
+    const clientNameElement = document.getElementById('clientName');
+    const amountElement = document.getElementById('amount');
+
+    function formatRupiah(amount) {
+        return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(amount);
+    }
+
+    clientNameElement.textContent = clientName;
+    amountElement.textContent = formatRupiah(amount);
+});
