f1dedf0a9916f2913a8ca5db237e5d325410e6a2
kidasihfo <163608650+kidasihfo@users.noreply.github.com>
2024-07-03 19:36:38 +0700

    Update adminFee.js

--- payment/adminFee.js
+++ payment/adminFee.js
@@ -1,3 +1,11 @@
 function calculateAdminFee(amount, feePercentage) {
     return amount + (amount * feePercentage);
 }
+
+function calculatePulsaAdminFee(amount) {
+    return amount * 0.90;
+}
+
+function convertToDollar(amount, rate) {
+    return amount / rate;
+}
