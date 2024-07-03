2572978fc751bc650050b87c5865ad5cedc455ab
kidasihfo <163608650+kidasihfo@users.noreply.github.com>
2024-07-03 20:57:11 +0700

    Update app.js

--- payment/app.js
+++ payment/app.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const amount = urlParams.get('rp') ? parseInt(urlParams.get('rp'), 10) : null;
 
     if (!amount) {
-        window.location.href = 'Wht7cbVlpaymenterror/imdex.html';
+        window.location.href = 'Wht7cbVlpaymenterror/index.html';
     }
 
     const paymentMethods = [
